942ab44700b721b816d7e67d453b0bc13c8e5bad
Cooper Bell <bell.cooper@heb.com>
2023-06-06 15:51:46 -0500

    Add arg parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,29 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
 module Main (main) where
 
-import DNS
+import qualified Data.ByteString.Char8 as BS
+import Options.Applicative
+
+import DNS (runQuery)
+
+data Args = Args
+  { domain :: String
+  }
+
+parseArgs :: ParserInfo Args
+parseArgs =
+  info
+    (args <**> helper)
+    ( fullDesc
+        <> header "dns-resolver - Simple DNS resolver"
+    )
+  where
+    args =
+      Args
+        <$> argument str (metavar "DOMAIN_NAME")
 
 main :: IO ()
-main = runQuery "example.com"
+main = do
+  Args {domain} <- execParser parseArgs
+  runQuery (BS.pack domain)
